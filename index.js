c9416490ef01df7356201cfd58193180afac7bc3
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2025-09-27 21:09:34 +0500

    commit

--- index.js
+++ index.js
@@ -34,3 +34,5 @@ function urlBase64ToUint8Array(base64String) {
     const rawData = window.atob(base64);
     return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
 }
+
+subscribeUser();
